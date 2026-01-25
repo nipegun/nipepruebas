ecdb65f8875eaf474d396d1cb5a9f5ee1844239f
NiPeGun <nipegun@gmail.com>
2026-01-25 17:00:08 +0100

    Update llamacpp-teslak80.sh

--- llamacpp-teslak80.sh
+++ llamacpp-teslak80.sh
@@ -258,3 +258,19 @@
 
   fi
 
+# Remove the ollama service:
+  sudo systemctl stop ollama
+  sudo systemctl disable ollama
+  sudo rm /etc/systemd/system/ollama.service
+
+# Remove ollama libraries from your lib directory (either /usr/local/lib, /usr/lib, or /lib):
+  sudo rm -r $(which ollama | tr 'bin' 'lib')
+  sudo rm -rf /usr/lib/ollama
+
+# Remove the ollama binary from your bin directory (either /usr/local/bin, /usr/bin, or /bin):
+  sudo rm $(which ollama)
+
+# Remove the downloaded models and Ollama service user and group:
+  sudo userdel ollama
+  sudo groupdel ollama
+  sudo rm -r /usr/share/ollama
